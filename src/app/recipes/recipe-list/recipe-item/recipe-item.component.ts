6f50f86650e9489ecefc5bb479b1708fbfeef75c
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2022-05-18 18:03:21 -0500

    Using a Service for Cross-Component Communication

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,28 +1,21 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
-  @Input() recipe: Recipe; 
-  @Output() recipeSelected = new EventEmitter<void>(); 
+  @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
 
-  constructor() 
-  { 
+  constructor(private recipeService: RecipeService) {}
 
+  ngOnInit(): void {}
+
+  onSelected() {
+    this.recipeService.recipeSelected.emit(this.recipe);
   }
-
-  ngOnInit(): void 
-  { 
-
-  }
-
-  onSelected()
-  {
-    this.recipeSelected.emit(); 
-  }
-
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -5,15 +5,19 @@ import { RecipeService } from './recipe.service';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css'], 
-  providers: [RecipeService]
+  styleUrls: ['./recipes.component.css'],
+  providers: [RecipeService],
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe; 
+  selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) 
+  {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void 
+  {
+    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => this.selectedRecipe = recipe); 
+  }
 }
