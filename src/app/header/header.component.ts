a22f7fde0bff9be425f3dbc10043b75b083bc7ee
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2022-05-19 08:55:10 -0500

    adding navigation to app

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output} from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
@@ -7,12 +7,5 @@ import { Component, EventEmitter, Output} from "@angular/core";
 
 export class HeaderComponent
 {
-    
-    // Create an event emitter
-    // include the output decorator so it can be listened to by parent
-    @Output() tabSelected = new EventEmitter<string>(); 
-    onSelect(tab: string)
-    {
-        this.tabSelected.emit(tab); 
-    }
+
 }
