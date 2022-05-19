57ba50e07b2d94214cb26e9226a8e7a60514b46f
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2022-05-19 09:51:41 -0500

    adding edit routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@
 // Imports
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
@@ -10,7 +13,13 @@ const appRoutes: Routes = [
     // path we will reach when we load the app for the first time
     // only redirect if the full path is empty
     { path: '', redirectTo: '/recipes', pathMatch: 'full'}, 
-    { path: 'recipes', component: RecipesComponent },
+    { path: 'recipes', component: RecipesComponent, children:
+    [
+        {path: '', component: RecipeStartComponent}, 
+        {path: 'new', component: RecipeEditComponent}, 
+        {path: ':id', component: RecipeDetailComponent}, 
+        {path: ':id/edit', component: RecipeEditComponent}
+    ]},
     { path: 'shopping-list', component: ShoppingListComponent},
 ];
 
